7116f7a97d190f910dc724d3dc26871f23df7bd9
aichouche AMIR <aichouche.amir@transformatek.dz>
2024-12-11 15:27:06 +0000

    Add second database to test

--- src/app/tests/test_api_test.py
+++ src/app/tests/test_api_test.py
@@ -1,72 +1,90 @@
 # -*- coding: utf-8 -*-
-import pytest
 import json
-from app import app  ,db
+
+import pytest
+
+from app import app, db
 from app.models.test_model import Test
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6dHJ1ZSwiaWF0IjoxNzMzNjQ2MzgwLCJqdGkiOiI4ZDkzYjRjYi1hZWE2LTQ4NjQtOTJjYS1hN2MxYzU2NzI4NzEiLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoxLCJuYmYiOjE3MzM2NDYzODAsImNzcmYiOiJkNWZhN2NjOS1kM2FjLTQ0ZDItYTUxMi01ZGNiNDk4NmZiMGIiLCJleHAiOjE3MzM3MzI3ODB9.wM7XgQnXyYHyEY0qmv3LsSEw51vECxnxn8qHgfSRN9A" 
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6dHJ1ZSwiaWF0IjoxNzMzODE5NDEyLCJqdGkiOiIwNDU5MTIwNC03YzFmLTQ4ODQtYWY5My1lODQ5ZWZhNTY2OGYiLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoxLCJuYmYiOjE3MzM4MTk0MTIsImNzcmYiOiIwOGNiNDQxNi1mMmM1LTQ4MmUtYmQ5MC1lYjk4YjcxNGI3MjUiLCJleHAiOjE3MzM5MDU4MTJ9.xAIUthTW_iuXYSFhu9iyhZyN5XByGpgRyvBlbU3nhpA"
+
 
 @pytest.fixture
 def client():
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "test.db")
     with app.test_client() as client:
-        yield client  
+        with app.app_context():
+            db.create_all() 
+        yield client
+        # with app.app_context():
+        #     db.drop_all() 
 
 
 
 def test_get_test_records(client):
     """Test GET TEST"""
-    
+
     response = client.get(
-        '/api/v1/test/',
-        headers={"Authorization": f"Bearer {token}"}
+        "/api/v1/test/", headers={"Authorization": f"Bearer {token}"}
     )
     assert response.status_code == 200
     data = json.loads(response.data)
-    assert "result" in data  
-    assert isinstance(data["result"], list) 
-    assert len(data["result"]) >= 1 
+    assert "result" in data
+    assert isinstance(data["result"], list)
+    assert len(data["result"]) >= 1
+
 
 def test_create_record(client):
     """Test POST"""
     payload = {"name": "fatima"}
     response = client.post(
-        '/api/v1/test/',
+        "/api/v1/test/",
         data=json.dumps(payload),
-        content_type='application/json',
-        headers={"Authorization": f"Bearer {token}"}
+        content_type="application/json",
+        headers={"Authorization": f"Bearer {token}"},
     )
-    assert response.status_code == 201 
+    assert response.status_code == 201
+
 
 def test_update_record(client):
     """Test PUT"""
-    record = db.session.query(Test).first()  
     payload = {"name": "aichouche"}
     response = client.put(
-        f'/api/v1/test/{2}',
+        f"/api/v1/test/{3}",
         data=json.dumps(payload),
-        content_type='application/json',
-        headers={"Authorization": f"Bearer {token}"}
+        content_type="application/json",
+        headers={"Authorization": f"Bearer {token}"},
     )
-    assert response.status_code == 200  
-
-
+    assert response.status_code == 200
 
 
 def test_delete_record(client):
     """Test DELETE"""
-    record = db.session.query(Test).first()  
     response = client.delete(
-        f'/api/v1/test/{4}',
-        content_type='application/json',
-        headers={"Authorization": f"Bearer {token}"}
+        f"/api/v1/test/{3}",
+        content_type="application/json",
+        headers={"Authorization": f"Bearer {token}"},
     )
-    assert response.status_code == 200  
+    assert response.status_code == 200
 
 
-    
 def test_greeting(client):
     """Test the greeting endpoint in ExampleApi."""
-    response = client.get('/api/v1/exampleapi/greeting') 
-    assert response.status_code == 200  
-    assert response.data.decode('utf-8') == "hello"
+    response = client.get("/api/v1/exampleapi/greeting")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "hello"
+
+
+def test_create_recordd(client):
+    """Test POST"""
+    payload = {"name": "dounia"}
+    response = client.post(
+        "/api/v1/test-test",
+        data=json.dumps(payload),
+        content_type="application/json",
+    )
+    assert response.status_code == 201
